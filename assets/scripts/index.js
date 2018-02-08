4231fefb1b42fff4f7ed99bcc68343e021d635d3
cjamespeirce <cjamespeirce@gmail.com>
2018-02-08 16:34:11 -0500

    Added logic Added switch player 1->2 function

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -8,6 +8,28 @@ $(() => {
 })
 
 const board = ['box1', 'box2', 'box3', 'box4', 'box5', 'box6', 'box7', 'box8', 'box9']
+console.log('board is on', board)
+
+const player1 = 'x'
+
+const player2 = 'o'
+// determine if playerX has chosen a box
+let currentPlayer = player1
+
+// if (condition) {
+//    statements1
+// } else {
+//    statements2
+// }
+
+// switch between players
+const switchPlayer = function () {
+  // if player1 then switch to player2
+  if (currentPlayer === player1) {
+    currentPlayer = player2
+  }
+}
+//switchPlayer()
 // use require with a reference to bundle the file and use it in this file
 // const example = require('./example')
 
